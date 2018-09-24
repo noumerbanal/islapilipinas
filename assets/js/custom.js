cadbdb3c2e252d3c609b18bcc615407e9c29d143
Noumer Ian Banal <noumer.banal@gmail.com>
2018-09-25 06:42:16 +0800

    v1.7

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,12 @@
-// Prevent from orphaned & widowed paraghraph
-$('h3').each(function(){
-    var string = $(this).html();
-    string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
-    $(this).html(string);
+$(document).ready(function(){
+
+	// Prevent from orphaned & widowed paraghraph
+	$('p').each(function(){
+	    var string = $(this).html();
+	    string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
+	    $(this).html(string);
+	});
+
+
 });
+

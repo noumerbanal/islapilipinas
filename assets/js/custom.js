248cd9ba3c0e83a669babdd2a1e83da0f7a0d6b2
= <=>
2018-09-25 15:22:04 +0800

    v1.8

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,6 +7,25 @@ $(document).ready(function(){
 	    $(this).html(string);
 	});
 
+	//Scroll Animation
+    $('#toTop').click(function(){ 
+        $('html,body').animate({ scrollTop: 0 }, 'slow');
+        return false; 
+    });
 
+    // Display to Top Button
+
+    if($(window).scrollTop() > 300) {
+    		$('.btn-top').fadeIn();
+	} else {
+		$('.btn-top').fadeOut();
+	}
+	
+    $(window).scroll(function(){
+    	if($(window).scrollTop() > 300) {
+    		$('.btn-top').fadeIn();
+    	} else {
+    		$('.btn-top').fadeOut();
+    	}
+	});
 });
-
